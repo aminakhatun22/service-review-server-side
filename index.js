e3fb5af1523260f8008c91e38ca387237ac13f70
aminakhatun22 <aminak26802@gmail.com>
2022-11-10 02:44:04 +0600

    added port

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+// middle wares
+app.use(cors())
+app.use(express.json());
+
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.a3goieg.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const serviceCollection = client.db('aahar').collection('services')
+    }
+
+}
+
+run().catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('aahar server side is running')
+})
+
+
+app.listen(port, () => {
+    console.log(`aahar server side running on ${port}`);
+})
