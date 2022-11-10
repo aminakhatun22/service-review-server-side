3886d4c3a6950d8799d0e29489d51a1282427fa8
aminakhatun22 <aminak26802@gmail.com>
2022-11-10 18:31:15 +0600

    dervices-details data api done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -25,18 +25,20 @@ console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+
+
 async function run() {
     try {
         const serviceCollection = client.db('aahar').collection('services');
         app.get('/services', async (req, res) => {
             const query = {}
-            const cursor = serviceCollection.find(query);
+            const cursor = serviceCollection.find(query).limit(3);
             const services = await cursor.toArray();
             res.send(services);
         });
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: Object(id) };
+            const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service);
         })
